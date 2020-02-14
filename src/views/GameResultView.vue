2793cb559d276fe0422e9ed81b619cf262e4f9e5
Lucia Serrano <lucias@spiceworks.com>
2020-02-14 01:55:02 -0600

    Updates to result views

--- src/views/GameResultView.vue
+++ src/views/GameResultView.vue
@@ -1,12 +1,12 @@
 <template>
   <main>
     <h1>The End</h1>
-    <h2>Congratulations, {{ players[0].name }},<br>developer extraordinaire.</h2>
+    <h2>Congratulations, {{ game.scores[0].name }},<br>developer extraordinaire.</h2>
 
     <h3>Final Standings</h3>
     <table>
       <tbody>
-        <tr v-for="player in players" :key="player">
+        <tr v-for="player in game.scores" :key="player.name">
           <td>{{ player.name }}</td>
           <td class="player-score">{{ player.score}}</td>
         </tr>
@@ -20,10 +20,7 @@
 export default {
   data() {
     return {
-      players: [ {name: 'klinsmann acosta', score: '4'},
-                 {name: 'anel', score: '2'},
-                 {name: 'bruno', score: '1'},
-                 {name: 'jd', score: '0'}],
+      game: this.$store.state.game
     }
   },
 }
